8732d3c3cea3456d544b944c87cc50bf1b672c2c
benliu91 <bliu36@dons.usfca.edu>
2017-05-02 12:47:26 -0700

    Add files via upload

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +32,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +52,43 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  VectorXd H_radar(3);
+
+  // recover state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // pre-compute a set of terms to avoid repeated calculation
+  float H0 = px*px+py*py;
+  float H1 = sqrt(H0);
+  float H2 = atan2(py,px);
+  float H3 = 0;
+
+  // check division by zero
+  if(fabs(H0) < 0.0001){
+    H3 = (px*vx+py*vy)/0.0001;
+  }
+  else{
+    H3 = (px*vx+py*vy)/H1;
+  }
+  H_radar << H1, H2, H3;
+
+  VectorXd y = z - H_radar; // z - h(x')
+
+  // phi adjusted to (-pi, pi)
+  while (y[1] < -M_PI) y[1] += 2 * M_PI; while (y[1] > M_PI) y[1] -= 2 * M_PI;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
